210882ed54b5a4ab379c5f98e8dda5371c8aa4cb
pakornds <73111732+pakornds@users.noreply.github.com>
2022-10-14 17:02:07 +0700

    dasdsa

--- public/js/background.js
+++ public/js/background.js
@@ -1,147 +1,62 @@
-(function () {
-  ShootingStar = function (id) {
-    this.n = 0;
-    this.m = 0;
-    this.defaultOptions = {
-      velocity: 8,
-      starSize: 10,
-      life: 300,
-      beamSize: 400,
-      dir: -1,
-    };
-    this.options = {};
-    id = typeof id != "undefined" ? id : "";
-    this.capa = $(id).lenght > 0 ? "body" : id;
-    this.wW = $(this.capa).innerWidth();
-    this.hW = $(this.capa).innerHeight();
-  };
+const area = document.querySelector(".galaxy");
 
-  ShootingStar.prototype.addBeamPart = function (x, y) {
-    this.n++;
-    var name = this.getRandom(100, 1);
-    $("#star" + name).remove();
-    $(this.capa).append("<div id='star" + name + "'></div>");
-    $("#star" + name).append(
-      "<div id='haz" +
-        this.n +
-        "' class='haz' style='position:absolute; color:#FF0; width:10px; height:10px; font-weight:bold; font-size:" +
-        this.options.starSize +
-        "px'>·</div>"
-    );
-    if (this.n > 1)
-      $("#haz" + (this.n - 1)).css({
-        color: "rgba(255,255,255,0.5)",
-      });
-    $("#haz" + this.n).css({
-      top: y + this.n,
-      left: x + this.n * this.options.dir,
-    });
-  };
+let amountMeteor = 15;
+let amountStar = 200;
+let i = 0;
+let j = 0;
+let k = 0;
 
-  ShootingStar.prototype.delTrozoHaz = function () {
-    this.m++;
-    $("#haz" + this.m).animate(
-      {
-        opacity: 0,
-      },
-      75
-    );
-    if (this.m >= this.options.beamSize) {
-      $("#ShootingStarParams").fadeOut("slow");
-    }
-  };
+function galaxy() {
+  while (i < amountMeteor) {
+    // meteor from top
+    let drop = document.createElement("i");
+    let size = Math.random() * 2 + 0.5;
+    let posX = Math.floor(Math.random() * area.offsetWidth);
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 20 + 3;
 
-  ShootingStar.prototype.getRandom = function (max, min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+    drop.style.width = size + "px";
+    drop.style.left = posX + "px";
+    drop.style.top = 0;
+    drop.style.animationDelay = delay + "s";
+    drop.style.animationDuration = duration + "s";
+    area.appendChild(drop);
+    i++;
+  }
 
-  ShootingStar.prototype.toType = function (obj) {
-    if (typeof obj === "undefined") {
-      return "undefined"; /* consider: typeof null === object */
-    }
-    if (obj === null) {
-      return "null";
-    }
-    var type =
-      Object.prototype.toString.call(obj).match(/^\[object\s(.*)\]$/)[1] || "";
-    switch (type) {
-      case "Number":
-        if (isNaN(obj)) {
-          return "nan";
-        } else {
-          return "number";
-        }
-      case "String":
-      case "Boolean":
-      case "Array":
-      case "Date":
-      case "RegExp":
-      case "Function":
-        return type.toLowerCase();
-    }
-    if (typeof obj === "object") {
-      return "object";
-    }
-    return undefined;
-  };
+  while (k < amountMeteor * 2) {
+    // meteor from right
+    let drop = document.createElement("i");
+    let size = Math.random() * 2 + 0.5;
+    let posY = Math.floor(Math.random() * area.offsetHeight);
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 30 + 3;
 
-  ShootingStar.prototype.launchStar = function (options) {
-    if (this.toType(options) != "object") {
-      options = {};
-    }
-    this.options = $.extend({}, this.defaultOptions, options);
-    this.n = 0;
-    this.m = 0;
-    var i = 0,
-      l = this.options.beamSize,
-      x = this.getRandom(this.wW - this.options.beamSize - 100, 100),
-      y = this.getRandom(this.hW - this.options.beamSize - 100, 100),
-      self = this;
-    for (; i < l; i++) {
-      setTimeout(function () {
-        self.addBeamPart(x, y);
-      }, self.options.life + i * self.options.velocity);
-    }
-    for (i = 0; i < l; i++) {
-      setTimeout(function () {
-        self.delTrozoHaz();
-      }, self.options.beamSize + i * self.options.velocity);
-    }
-    $("#ShootingStarParams").html(
-      "Launching shooting star. PARAMS: wW: " +
-        this.wW +
-        " - hW: " +
-        this.hW +
-        " - life: " +
-        this.options.life +
-        " - beamSize: " +
-        this.options.beamSize +
-        " - velocity: " +
-        this.options.velocity
-    );
-    $("#ShootingStarParams").fadeIn("slow");
-  };
+    drop.style.width = size + "px";
+    drop.style.right = 0;
+    drop.style.top = posY + "px";
+    drop.style.animationDelay = delay + "s";
+    drop.style.animationDuration = duration + "s";
+    area.appendChild(drop);
+    k++;
+  }
 
-  ShootingStar.prototype.launch = function (everyTime) {
-    if (this.toType(everyTime) != "number") {
-      everyTime = 10;
-    }
-    everyTime = everyTime * 1000;
-    this.launchStar();
-    var self = this;
-    setInterval(function () {
-      var options = {
-        dir: self.getRandom(1, 0) ? 1 : -1,
-        life: self.getRandom(400, 100),
-        beamSize: self.getRandom(700, 400),
-        velocity: self.getRandom(10, 4),
-      };
-      self.launchStar(options);
-    }, everyTime);
-  };
-})();
+  while (j < amountStar) {
+    let star = document.createElement("j");
+    let size = Math.random() * 5;
+    let posX = Math.floor(Math.random() * area.offsetWidth * 2);
+    let posY = Math.floor(Math.random() * area.offsetHeight);
+    let duration = Math.random() * 300 + 15;
 
-$(document).ready(function () {
-  var shootingStarObj = new ShootingStar("body");
-  shootingStarObj.launch();
-});
+    star.style.width = size + "px";
+    star.style.height = size + "px";
+    star.style.left = posX + "px";
+    star.style.top = posY + "px";
+    star.style.animationDuration = duration + "s";
+
+    area.appendChild(star);
+    j++;
+  }
+}
+
+galaxy();
