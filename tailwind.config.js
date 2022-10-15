2bb05bb455132a503b8423e8768108a87f8f6290
pakornds <73111732+pakornds@users.noreply.github.com>
2022-10-15 20:05:21 +0700

    dsadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     screens: {
-      sm: { min: "640px", max: "767px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
+      sm: { min: "640px" },
+      // => @media (min-width: 640px
 
-      md: { min: "768px", max: "1023px" },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
+      md: { min: "768px" },
+      // => @media (min-width: 768px
 
-      lg: { min: "1024px", max: "1279px" },
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+      lg: { min: "1024px" },
+      // => @media (min-width: 1024px
 
       xl: { min: "1280px" },
       // => @media (min-width: 1280px
